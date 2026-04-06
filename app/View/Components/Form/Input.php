81e11aabcfbc4d6b73999f15c50e9ea2b1a49d40
fisbaduh04 <fisabduh04@gmail.com>
2026-04-06 14:55:05 +0700

    fix: ubah case sensitivity nama file komponen Input & Session agar bisa dibaca server Linux

--- app/View/Components/Form/Input.php
+++ app/View/Components/Form/Input.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class input extends Component
+class Input extends Component
 {
     public $id;
     public $name;
